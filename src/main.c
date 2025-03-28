3cdc392a487f7fa4e6bfef479124d20cb2d90b5d
Henix212 <hugogpdealmeida@gmail.com>
2025-03-28 20:30:43 +0100

    Fix multiple definition I2C_Init

--- src/main.c
+++ src/main.c
@@ -1,4 +1,17 @@
+#include "I2C.h"
+
 int main(int argc, char const *argv[])
-{
-    
+{   
+    // Initialize the I2C peripheral
+    if (I2C_Init() != 1) {
+        // Initialization failed, handle error
+        while (1);
+    }
+
+    // Main loop
+    while (1) {
+        // Your main code here
+    }
+
+    return 0;
 }
