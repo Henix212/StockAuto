b635f384e5565e5100d88c45e66f677ef135ad7a
Henix212 <hugogpdealmeida@gmail.com>
2025-04-02 17:33:36 +0200

    Working Servo

--- src/main.c
+++ src/main.c
@@ -1,4 +1,21 @@
+#include "stm32g4xx_hal.h"
+#include "servoMoteur.h"
 int main(int argc, char const *argv[])
 {
+    HAL_Init();  // Initialisation de la HAL
 
+    INIT_servo();
+
+    int position = 0;
+
+    while(1){
+        while (position <= 100){
+            position += 3;
+            SET_position(position);
+            HAL_Delay(10);  // Attendre 1 seconde
+        }
+        HAL_Delay(250);  // Attendre 1 seconde
+        position = 0;
+        HAL_Delay(2000);
+    }
 }
