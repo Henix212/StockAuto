d280284bfe808cd3b0d8bb905fafdc90c4b65ce8
floalzaix <floalzfencing@gmail.com>
2025-04-15 14:41:08 +0200

    Supression du reste

--- src/main.c
+++ src/main.c
@@ -1,17 +1,4 @@
-#include "I2C.h"
-
 int main(int argc, char const *argv[])
 {   
-    // Initialize the I2C peripheral
-    if (I2C_Init() != 1) {
-        // Initialization failed, handle error
-        while (1);
-    }
-
-    // Main loop
-    while (1) {
-        // Your main code here
-    }
-
-    return 0;
+    
 }
