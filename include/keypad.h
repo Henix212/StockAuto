135bd9081d1fc5c808d5cbb0c7ed8ace3e06923b
floalzaix <floalzfencing@gmail.com>
2025-05-28 10:34:44 +0200

    Init new main

--- include/keypad.h
+++
@@ -1,7 +0,0 @@
-#ifndef KEYPAD_H
-#define KEYPAD_H
-
-volatile char last_key = 0;
-int keypad_getkey(void);
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,41 +1,5 @@
 #include "main.h"
-#include "keypad.h"
-
-void Error_Handler(void) {
-    while (1) {}
-}
 
 int main(void) {
     HAL_Init();
-
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    GPIO_InitTypeDef gpio = {0};
-
-    // Lignes = sorties
-    gpio.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5;
-    gpio.Mode = GPIO_MODE_OUTPUT_PP;
-    gpio.Pull = GPIO_NOPULL;
-    gpio.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOA, &gpio);
-
-    // Colonnes = entrées avec pull-up
-    gpio.Pin = GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9;
-    gpio.Mode = GPIO_MODE_IT_FALLING;
-    gpio.Pull = GPIO_PULLUP;
-    HAL_GPIO_Init(GPIOA, &gpio);
-
-    HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
-
-    SystemClock_Config();
-
-    while (1) {
-    if (last_key != 0) {
-        // Une touche a été pressée
-        if (last_key == '1') {
-            // Action
-        }
-        last_key = 0; // Reset après traitement
-        }
-    }
 }
